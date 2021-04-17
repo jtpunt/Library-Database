4352ef4b41866b41b22a33f4e8131448f667737d
jtpunt <jonathan.perry1994@gmail.com>
2021-04-17 04:12:57 -0500

    added bash script in WSL-Ubuntu to open every program needed for nodejs web development

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# to be used in WSL-Ubuntu to open every program needed for nodejs web development
+cd ../../mnt/c/"Documents and Settings"/"Jonathan Perry"/Documents/GitHub
+subl.exe Library-Database
+github Library-Database
+sensible-browser http://localhost:3005/
+node.exe Library-Database/server.js
