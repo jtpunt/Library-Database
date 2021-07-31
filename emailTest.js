260593876fb71aa9018e93c74ff1394ae8ca44ca
jtpunt <jonathan.perry1994@gmail.com>
2021-07-31 18:59:09 -0500

    Update emailTest.js

--- emailTest.js
+++ emailTest.js
@@ -1,5 +1,10 @@
-const Email = require('email-templates');
-
+const Email = require('email-templates'),
+    dotenv = require('dotenv');
+dotenv.config();
+// If you are getting authentication problems while using gmail as the mail
+// transport host, go to your Gmail account, click 'Manage your Google Account',
+// click the 'Security' tab, then scroll down to the bottom and switch
+// 'Less secure app access' to on
 const email = new Email({
     message: {
         from: 'jonathan.perry1994@gmail.com'
@@ -9,10 +14,14 @@ const email = new Email({
     transport: {
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
+        secure: false,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS,
         },
+        tls: {
+            rejectUnauthorized: false
+        }
     },
 });
 
